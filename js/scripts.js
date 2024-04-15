8bb765e897e4363bab11256707984ac848d0f988
eryncraig <erynlcraig@gmail.com>
2024-04-15 13:37:28 -0700

    added jquery functionality so users can add, delete, and cross out list items as well as rearrange them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,36 @@
 function newItem() {
 
+  let list = $('#list');
   let li = $('<li></li>');
-  let inputValue = $('#input').value;
+  let inputValue = $('#input').val();
   let text = document.createTextNode(inputValue);
 
-  li.appendChild(text);
+  li.append(text);
+
+  if (inputValue === '') {
+    alert('you must write something!')
+  } else {
+    list.append(li);
+  }
+
+
+  function crossOut() {
+    li.toggleClass('strike')
+  }
+
+  li.dblclick(crossOut);
+
+  let deleteButton = $('<button></button>');
+
+  deleteButton.append(document.createTextNode('X'));
+  li.append(deleteButton);
+
+  deleteButton.on('click', deleteListItem);
+
+  function deleteListItem() {
+    li.addClass('delete');
+  }
+
+  list.sortable();
+
 }
